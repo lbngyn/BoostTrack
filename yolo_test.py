15b8ca52478aaa114360f3d68c86afb1947e4c11
lngy <lebinhnguyen2004@gmail.com>
2024-12-17 22:02:07 +0700

    fix visualize_videos.py

--- yolo_test.py
+++ yolo_test.py
@@ -24,6 +24,7 @@ def display_results(img, results):
     """
     for box in results:
         x1, y1, x2, y2, conf = box
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
         print(box)
         cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
         cv2.putText(img, f'{conf:.2f}', (x1, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
