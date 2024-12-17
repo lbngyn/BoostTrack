a248d5d2f517d00bc97e5b6f39b2d149d051b547
lngy <lebinhnguyen2004@gmail.com>
2024-12-17 16:16:28 +0700

    yolov10x

--- external/yolov10.py
+++ external/yolov10.py
@@ -72,7 +72,7 @@ class YoloV10Detector:
         labels = np.array(labels)
 
         # Chuyển bounding boxes về kích thước ảnh gốc
-        h, w = img_shape
+        w, h = img_shape
         scale_w, scale_h = w / self.img_size[1], h / self.img_size[0]
         boxes[:, [0, 2]] *= scale_w
         boxes[:, [1, 3]] *= scale_h
