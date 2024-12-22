7401619a76f6b550825376545890ceea2cfccc0a
lngy <lebinhnguyen2004@gmail.com>
2024-12-22 22:52:17 +0700

    update use general setting

--- visualize_video.py
+++ visualize_video.py
@@ -2,10 +2,39 @@ import argparse
 import cv2
 from tracker.boost_track import BoostTrack
 import utils
-from default_settings import GeneralSettings, get_detector_path_and_im_size
+import os
+from args import make_parser
+from default_settings import GeneralSettings, get_detector_path_and_im_size, BoostTrackPlusPlusSettings, BoostTrackSettings
 import torch
 from external.yolov10 import YoloV10Detector
 import random
+
+def get_main_args():
+    parser = make_parser()
+    parser.add_argument("--dataset", type=str, default="mot17")
+    parser.add_argument("--result_folder", type=str, default="results/trackers/")
+    parser.add_argument("--test_dataset", action="store_true")
+    parser.add_argument("--exp_name", type=str, default="test")
+    parser.add_argument("--no_reid", action="store_true", help="mark if visual embedding should NOT be used")
+    parser.add_argument("--no_cmc", action="store_true", help="mark if camera motion compensation should NOT be used")
+
+    parser.add_argument("--s_sim_corr", action="store_true", help="mark if you want to use corrected version of shape similarity calculation function")
+
+    parser.add_argument("--btpp_arg_iou_boost", action="store_true", help="BoostTrack++ arg. Mark if only IoU should be used for detection confidence boost.")
+    parser.add_argument("--btpp_arg_no_sb", action="store_true", help="BoostTrack++ arg. Mark if soft detection confidence boost should NOT be used.")
+    parser.add_argument("--btpp_arg_no_vt", action="store_true", help="BoostTrack++ arg. Mark if varying threshold should NOT be used for the detection confidence boost.")
+
+    parser.add_argument("--no_post", action="store_true", help="do not run post-processing.")
+
+    args = parser.parse_args()
+    if args.dataset == "mot17":
+        args.result_folder = os.path.join(args.result_folder, "MOT17-val")
+    elif args.dataset == "mot20":
+        args.result_folder = os.path.join(args.result_folder, "MOT20-val")
+
+    if args.test_dataset:
+        args.result_folder.replace("-val", "-test")
+    return args
 
 def preprocess_image(image, input_size):
     h, w = input_size
@@ -16,7 +45,7 @@ def preprocess_image(image, input_size):
 def generate_random_color():
     return random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
 
-def process_video(video_path, output_path, model_path, det_classes):
+def process_video(video_path, output_path, model_path, det_classes, arg):
     size = (800, 1440)
     det = YoloV10Detector(model_path=model_path, img_size=size)
     print(next(det.model.parameters()).device)
@@ -57,7 +86,7 @@ def process_video(video_path, output_path, model_path, det_classes):
             continue
 
         targets = tracker.update(pred, frame_rgb_tensor, frame_rgb, tag=f"frame_{frame_count}")
-        tlwhs, ids, confs = utils.filter_targets(targets, 1.6, 10)
+        tlwhs, ids, confs = utils.filter_targets(targets, GeneralSettings.conf_thresh, GeneralSettings.min_box_area)
         for tlwh, track_id, conf in zip(tlwhs, ids, confs):
             x1, y1, w, h = map(int, tlwh)
 
@@ -79,12 +108,24 @@ def process_video(video_path, output_path, model_path, det_classes):
     print(f"Output saved to: {output_path}")
 
 if __name__ == "__main__":
+    args = get_main_args()
+    GeneralSettings.values['dataset'] = args.dataset
+    GeneralSettings.values['use_embedding'] = not args.no_reid
+    GeneralSettings.values['use_ecc'] = not args.no_cmc
+    GeneralSettings.values['test_dataset'] = args.test_dataset
+
+    BoostTrackSettings.values['s_sim_corr'] = args.s_sim_corr
+
+    BoostTrackPlusPlusSettings.values['use_rich_s'] = not args.btpp_arg_iou_boost
+    BoostTrackPlusPlusSettings.values['use_sb'] = not args.btpp_arg_no_sb
+    BoostTrackPlusPlusSettings.values['use_vt'] = not args.btpp_arg_no_vt
+
     parser = argparse.ArgumentParser(description="Multi-Object Tracking Pipeline")
     parser.add_argument("--video", type=str, default="/kaggle/input/mot17-videos/MOT17-02-FRCNN-raw.mp4", help="Path to input video")
     parser.add_argument("--output", type=str, default="/kaggle/working/MOT17-02-FRCNN-raw.mp4", help="Path to save output video")
     parser.add_argument("--model", type=str, default="/kaggle/input/yolov10x/other/default/1/yolov10x.pt", help="Path to YOLOv10x model")
     parser.add_argument("--det_classes", nargs="+", type=int, default=[0], help="Detection classes (default: [0])")
 
-    args = parser.parse_args()
+    args2 = parser.parse_args()
 
-    process_video(args.video, args.output, args.model, args.det_classes)
+    process_video(args2.video, args2.output, args2.model, args2.det_classes, args)
